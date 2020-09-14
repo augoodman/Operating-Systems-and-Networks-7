839191ea0ec118131999ef36d782a6a7cb50a56c
augoodma <augoodma@asu.edu>
2020-09-13 20:06:37 -0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/Module7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module7.dir/main.c.o"
+  "Module7"
+  "Module7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Module7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
